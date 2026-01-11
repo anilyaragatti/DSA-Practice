4041f53ef919e7c921e79ed2482aaa1503188a30
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-11 23:53:34 +0530

    dsa

--- basic.java
+++ basic.java
@@ -1,26 +1,18 @@
- public class basic {
+public class basic {
 
-   public static void reverse(int numbers[]){
-      int first=0; int last=numbers.length-1;
-      while (first<last) {
-         int temp = numbers[first];
-         numbers[first]=numbers[last];
-         numbers[last]=temp;
-
-         first++;
-         last--;
-         
+  public static void pairs(int numbers[]){
+    for(int i=0;i<numbers.length;i++){
+      int curr = numbers[i];
+      for(int j=i+1;j<numbers.length;j++){
+        System.out.print("("+curr+","+numbers[j]+") ");
       }
-      
-   }
- 
-   public static void main(String[] args) {
-      int numbers[] = {2,4,8,10};
-       reverse(numbers);
+      System.out.println();
+    }
+  }
 
-       for(int i=0;i<numbers.length;i++){
-         System.out.print(numbers[i]+" ");
-       }
- System.out.println(" ");
-   }
- }
+  public static void main(String[] args) {
+    int numbers[] = {2,4,6,8,10};
+    pairs(numbers);
+
+  }
+}

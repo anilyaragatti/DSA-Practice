b72d9c05f7bfb639dbcc11eae7573055372a05f9
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-12 23:21:15 +0530

    2nd DAY

--- basic.java
+++ basic.java
@@ -1,18 +1,4 @@
-public class basic {
-
-  public static void pairs(int numbers[]){
-    for(int i=0;i<numbers.length;i++){
-      int curr = numbers[i];
-      for(int j=i+1;j<numbers.length;j++){
-        System.out.print("("+curr+","+numbers[j]+") ");
-      }
-      System.out.println();
-    }
-  }
-
-  public static void main(String[] args) {
-    int numbers[] = {2,4,6,8,10};
-    pairs(numbers);
-
-  }
-}
+ public class basic {
+ 
+  
+ }

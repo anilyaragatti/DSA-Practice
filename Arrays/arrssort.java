504d5308b60b35026aca0e7346bc038ff7b321bd
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-14 22:50:09 +0530

    dsa4

--- Arrays/arrssort.java
+++ Arrays/arrssort.java
@@ -1,11 +1,23 @@
 package Arrays;
 
-     
-
 public class arrssort {
-    public static int 
-    public static void main(String[] args) {
-        int numners[] = {2,3,4,5,6,7,8,9,};
+
+    public static boolean sort(int numbers[]){
+        for(int i=0;i<numbers.length-1;i++){
+            if(numbers[i]>numbers[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
-    
+
+    public static void main(String[] args) {
+        int numbers[] = {2,3,7,6};
+        if(sort(numbers)){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("flase");
+        }
+    }
 }

82cb0a9b02f491220029f86b644f1bd3aa4e000d
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-17 23:35:56 +0530

    dsa6

--- Arrays/maxSunArray.java
+++ Arrays/maxSunArray.java
@@ -1,27 +1,29 @@
-public class arrssort {
+package Arrays;
 
-    public static void maxsum(int numbers[]) {
+public class maxSunArray {
+
+    public static void maxSubArray(int numbers[]) {
         int cursum = 0;
-        int maxsum = Integer.MIN_VALUE;
-
+        int maxSub = Integer.MIN_VALUE;
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i; j < numbers.length; j++) {
                 cursum = 0;
                 for (int k = i; k <= j; k++) {
-
                     cursum += numbers[k];
+
                 }
                 System.out.println(cursum);
-                if (cursum > maxsum) {
-                    maxsum = cursum;
+                if (maxSub < cursum) {
+                    maxSub = cursum;
                 }
             }
         }
-        System.out.println("THE MAX = " + maxsum);
+        System.out.println("MAX SUM = " + maxSub);
     }
 
     public static void main(String[] args) {
         int numbers[] = { 2, 4, 6, 8, 10 };
-        maxsum(numbers);
+        maxSubArray(numbers);
     }
+
 }
---
+++ Arrays/prefixSum.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class prefixSum {
+
+    public static void  prefixSum(int numbers[]){
+        int curSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0]=numbers[0];
+        //calculate prefix array
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+numbers[i];
+        }
+
+
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i;j<numbers.length;j++){
+                curSum= i==0 ? prefix[j]: prefix[j]-prefix[i-1];
+                if(maxSum<curSum){
+                    maxSum=curSum;
+                }
+            }
+        }
+        System.out.println("MAX SUM = "+ maxSum);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        prefixSum(numbers);
+    }
+    
+}

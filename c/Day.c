71698b1b8417c411a77c5d736e4fa6b9e1b58c1f
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-18 14:50:32 +0530

    dsapro1Com

--- c/Day.c
+++ c/Day.c
@@ -24,3 +24,31 @@ struct Day createDay()
     scanf("%s",day.activity);
     return day;
 };
+
+void read(struct Day calender[7]){
+    int numDay = 0;
+    while (numDay<7){
+        printf("Enter datails for day %d", numDay+1);
+        struct Day newDay = createDay();
+        calender[numDay] = newDay;
+        numDay++;
+    }   
+}
+
+void display(struct Day calender[7]){
+    printf("calendar for week:");
+    for(int i=0;i<7;i++){
+        printf("Day %d: %s, Date: %d", i+1,calender[i].name,calender[i].date);
+        printf("activity %s: ", calender[i].activity);
+
+    }
+}
+
+int main(){
+    struct Day calender[7];
+    printf('create a calendar for week');
+    read(calender);
+    display(calender);
+    return 0;
+
+}

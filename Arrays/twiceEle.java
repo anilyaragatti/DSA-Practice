e2adcb49debb6b14f256848cad9ad7e4afda5aed
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-23 20:09:52 +0530

    twiceElements

---
+++ Arrays/twiceEle.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class twiceEle {
+
+    public static boolean twiceElement(int number[]){
+        for(int i=0;i<number.length;i++){
+            for(int j=i+1;j<number.length;j++){
+                if(number[i]==number[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int number[] = {1,2,3};
+        if(twiceElement(number)){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+    }
+}

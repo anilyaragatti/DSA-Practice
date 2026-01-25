f3d97a5896eb3695d5a42e149e0ffa7bbdbfff71
Anil yaragatti <yaragattianil8@gmail.com>
2026-01-26 02:17:23 +0530

    selection sort

--- Arrays/selectionSort.java
+++ Arrays/selectionSort.java
@@ -5,8 +5,8 @@ public class selectionSort {
     public static void selectionsort(int number[]){
         for(int i=0;i<number.length-1;i++){
             int minPos = i;
-            for(int j=i+1;j<number.length;j++){
-                if(number[minPos]>number[j]){
+            for(int j=i+1;j>number.length;j++){ 
+                if(number[minPos]<number[j]){
                     minPos=j;
                 }
             }

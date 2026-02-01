6a9db832d1f8aa50f9a54aab407f7a355db5cea5
Anil yaragatti <yaragattianil8@gmail.com>
2026-02-01 23:56:50 +0530

    dsa

--- primeno.java
+++ primeno.java
@@ -1,26 +1,27 @@
- 
-
 
 import java.util.*;
+
 public class primeno {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        boolean isprime  = true;
+        boolean isprime = true;
 
-        for(int i=2;i<n-1;i++){
-            if(n % 1 ==0){
+        for (int i = 2; i < n - 1; i++) {
+            if (n % 1 == 0) {
                 isprime = false;
             }
         }
-        if(isprime==true){
+        if (isprime == true) {
             System.out.println("n is prime number");
-        }else{
+        } else {
             System.out.println("n is even number");
+            System.out.println("n is even number");
+
         }
         sc.close();
-        
+
     }
 }

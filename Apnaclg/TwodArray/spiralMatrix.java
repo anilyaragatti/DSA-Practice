3283ba885d48c2c52fbcd04924471b697d5eaa77
Anil yaragatti <yaragattianil8@gmail.com>
2026-02-12 20:26:04 +0530

    spiral

---
+++ Apnaclg/TwodArray/spiralMatrix.java
@@ -0,0 +1,16 @@
+package TwodArray;
+
+public class spiralMatrix {
+
+    public static printSpiral(int matrix[][]){
+        int startRow=0;
+        int startCol=0;
+        int endRow=matrix.length-1;
+        int endCol=matrix[0].length-1;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    }
+
+}

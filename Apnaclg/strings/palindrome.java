9e40f53e12ba0f99465e69f626998250b3b0160c
Anil yaragatti <yaragattianil8@gmail.com>
2026-02-18 23:31:27 +0530

    palindrome

---
+++ Apnaclg/strings/palindrome.java
@@ -0,0 +1,23 @@
+package strings;
+
+ 
+
+public class palindrome {
+
+    public static boolean checkPalindrome(String str){
+        for(int i=0;i<str.length()/2;i++){
+            int n=str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "racepar";
+        System.out.println(checkPalindrome(str));
+    }
+    
+}

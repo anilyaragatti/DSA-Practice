a79d3a3807a281f9032005fd985469989eaf5928
Anil yaragatti <yaragattianil8@gmail.com>
2026-02-28 12:44:00 +0530

    reverse

---
+++ Apnaclg/arrayList/findMax.java
@@ -0,0 +1,25 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class findMax {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        list.add(3);
+        list.add(10);
+        list.add(25);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+        System.out.println(max);
+
+    }
+
+}

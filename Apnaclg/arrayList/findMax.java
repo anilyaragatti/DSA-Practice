ef63ede0b8ee6bfd68ed4a6021708eb090a50f9b
Anil yaragatti <yaragattianil8@gmail.com>
2026-02-28 12:46:33 +0530

    dsa

--- Apnaclg/arrayList/findMax.java
+++ Apnaclg/arrayList/findMax.java
@@ -14,9 +14,11 @@ public class findMax {
 
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) > max) {
-                max = list.get(i);
-            }
+            // if (list.get(i) > max) {
+            //     max = list.get(i);
+            // }
+
+            max=Math.max(max, list.get(i));
         }
         System.out.println(max);
 

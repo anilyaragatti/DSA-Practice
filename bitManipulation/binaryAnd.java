76a2e895898544aee0b792904aff20de55016749
Anil yaragatti <yaragattianil8@gmail.com>
2026-03-02 19:08:55 +0530

    bit

--- bitManipulation/binaryAnd.java
+++ bitManipulation/binaryAnd.java
@@ -3,8 +3,9 @@ package bitManipulation;
 public class binaryAnd {
 
     public static void main(String[] args) {
-        System.out.println((5 & 6));
-        System.out.println((5 | 6));
+        System.out.println((5 & 6)); //Bit-wise AND
+        System.out.println((5 | 6)); //Bit-wise OR
+        System.out.println((5 ^ 6)); //Bit-wise XOR
 
     }
 }

443d2111be09c55cfd6ec24ec40b0606cb411e2f
Anil yaragatti <yaragattianil8@gmail.com>
2026-03-03 16:17:18 +0530

    bit manipulations

--- bitManipulation/binaryAnd.java
+++ bitManipulation/binaryAnd.java
@@ -6,6 +6,9 @@ public class binaryAnd {
         System.out.println((5 & 6)); //Bit-wise AND
         System.out.println((5 | 6)); //Bit-wise OR
         System.out.println((5 ^ 6)); //Bit-wise XOR
+        System.out.println((~5));//once Complement
+        System.out.println(5<<2);//Binry Left-Shift
+        System.out.println(6>>1);//Binary Rigth-Shift
 
     }
 }
